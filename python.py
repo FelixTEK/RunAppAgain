746dbcaa3b7ffd2f2fbaa5746f02fa46f0e2f8d7
FelixTEK <62253541+FelixTEK@users.noreply.github.com>
2022-04-11 15:49:21 +0700

    Add files via upload

---
+++ python.py
@@ -0,0 +1,24 @@
+#FELIXTEK 2022
+argument = ""
+
+def function():
+  print("Lorem ipsum dolor sit amet, consectetur adipiscing elit.")   #function properties
+  runagain()
+
+def runagain():
+  global argument
+  argument = input("Run again? (Y/N)\n> ").capitalize()
+  if argument == "Y":
+    function()
+  elif argument == "N":
+    print("Bye.")
+  else:
+    print("Invalid response.")
+    runagain()
+
+if __name__ == "__main__": #main function
+  while True:
+    function()
+    if argument == "N":
+      break
+    
